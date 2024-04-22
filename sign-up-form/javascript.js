c650bdd51cb62d5b64f6201bec01138c459c1f61
Nick Kalas <nick.kalas@proton.me>
2024-04-22 16:35:22 +0300

    Refactor pass 2

--- sign-up-form/javascript.js
+++ sign-up-form/javascript.js
@@ -3,26 +3,28 @@ let inputs = document.querySelectorAll('input');
 let errorMessages = document.querySelectorAll('span');
 let password1 = document.querySelector('#password');
 let password2 = document.querySelector('#confirm-pwd');
-let passwordError = document.querySelector('.password :last-child:not([class]):not([id])');
+let passwordError = document.querySelector('.password :last-child');
 
-function showError(element, message) {
+function showError(element, input, message) {
     element.textContent = message;
     element.classList.add('show-error');
+    input.style.cssText = "border-color: red";
 }
 
-function hideError(element) {
+function hideError(element, input) {
     element.textContent = "";
     element.classList.remove('show-error');
+    input.style.cssText = "";
 }
 
 function validateInputs() {
     let isValid = true;
     inputs.forEach(function(input, index) {
         if (!input.checkValidity()) {
-            showError(errorMessages[index], "* This field is required");
+            showError(errorMessages[index], input, "* This field is required");
             isValid = false;
         } else {
-            hideError(errorMessages[index]);
+            hideError(errorMessages[index], input);
         }
     });
     return isValid;
@@ -31,10 +33,10 @@ function validateInputs() {
 function validatePasswords() {
     let isValid = true;
     if (password1.value !== password2.value) {
-        showError(passwordError, "* Passwords do not match");
+        showError(passwordError, password1, "* Passwords do not match");
         isValid = false;
     } else if (password1.value !== "") {
-        hideError(passwordError);
+        hideError(passwordError, password1);
     }
     return isValid;
 }
@@ -52,3 +54,4 @@ form.addEventListener('submit', function(event) {
     }
 });
 
+// Add functionality for showing and hiding the passwords with an eye
