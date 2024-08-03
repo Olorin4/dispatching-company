5d3c5c69bbcce46b0b0024cbbcc7fdbb884cd9c9
Nick Kalas <nick.kalas@proton.me>
2024-08-03 21:11:43 +0300

    Import image-carousel

---
+++ index.js
@@ -0,0 +1,6 @@
+import { initImageCarousel } from 'component-image-carousel';
+
+// Initialize the image carousel when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+    initImageCarousel();
+});
