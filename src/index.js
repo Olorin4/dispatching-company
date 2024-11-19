dd56eb5ba40dba92a05c91fec3879081c20064be
Nick Kalas <nick.kalas@proton.me>
2024-11-19 19:27:49 +0200

    Remove carousel import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-import '../node_modules/component-image-carousel';
-
 // Initialize the image carousel when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', () => {
-    initImageCarousel();
-});
+document.addEventListener("DOMContentLoaded", () => {});
